02c69d71876e7baf2807f1e82d7ca34ab9712d24
patbrown <patbrown>
1992-12-09 18:31:50 +0000

    Removed FFTSCN from dummy routines.  Also, removed extra space.  (patbrown)

--- lbldum_b.f
+++ lbldum_b.f
@@ -2,25 +2,21 @@ C     path:      %P%
 C     revision:  $Revision$
 C     created:   $Date$  
 C     presently: %H%  %T%
-      SUBROUTINE NONLTE (MPTS)                                                  
-      PRINT * , ' NONLTE NOT IMPLEMENTED'                                       
-      STOP                                                                      
-      END                                                                       
-      SUBROUTINE XLAYMS(MPS,NPTS,LFILE,MFILE,NFILE)                             
-      PRINT * , ' XLAYMS NOT IMPLEMENTED'                                       
-      STOP                                                                      
-      END                                                                       
+      SUBROUTINE NONLTE (MPTS)
+      PRINT * , ' NONLTE NOT IMPLEMENTED'
+      STOP
+      END
+      SUBROUTINE XLAYMS(MPS,NPTS,LFILE,MFILE,NFILE)
+      PRINT * , ' XLAYMS NOT IMPLEMENTED'
+      STOP
+      END
       SUBROUTINE LASER(VLAS,MFILE,JAERSL)
-      PRINT * , ' LASER NOT IMPLEMENTED'                                        
-      STOP                                                                      
-      END                                                                       
-      FUNCTION RANDM(IRAND)                                                  
-      PRINT * , ' RANDM NOT IMPLEMENTED PROPERLY'                              
-      RANDM=0.5                                                                
-      IRAND=IABS(IRAND)                                                         
-      RETURN                                                                    
-      END                                                                       
-      SUBROUTINE FFTSCN(IFILE,JFILE)
-      PRINT * , ' FFTSCN NOT IMPLEMENTED'        
-      STOP      
-      END       
+      PRINT * , ' LASER NOT IMPLEMENTED'
+      STOP
+      END
+      FUNCTION RANDM(IRAND)
+      PRINT * , ' RANDM NOT IMPLEMENTED PROPERLY'
+      RANDM=0.5
+      IRAND=IABS(IRAND)
+      RETURN
+      END
