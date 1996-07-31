09a3a6dea1c0b68dd7c8c36ac8a8af27c309d818
patbrown <patbrown>
1996-07-31 15:11:14 +0000

    Reflects version 1.7 of lbl2r.f, which added the line ITYPE=2. (pbrown)

--- rrtatm.f
+++ rrtatm.f
@@ -5,7 +5,7 @@ C     presently: 03/18/96  18:32:37
       SUBROUTINE RRTATM
 C
 C     This routine has been modified from lblatm.f for use with RRTM,
-C      using the translation code, lbl2r.f, version 1.6
+C      using the translation code, lbl2r.f, version 1.7
 C
 C     path:      %P%
 C     revision:  $Revision$
@@ -534,6 +534,7 @@ C                                                                        FA04820
          READ (IRD,900) MODEL,ITYPE,IBMAX,NOZERO,NOPRNT,NMOL,IPUNCH,     FA04830
      *                  IFXTYP,MUNITS,RE,HSPACE,XVBAR,CO2MX              FA04840
          XVBAR = 1.0
+         ITYPE = 2
       ENDIF                                                              FA04850
 C                                                                        FA04860
       NOP = NOPRNT                                                       FA04870
